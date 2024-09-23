383a6f3d5364f1fa072e24a78469df15b82d62a8
j.vandiepen <22joshua2005@gmail.com>
2024-09-23 11:55:45 +0200

    web app fix (newest)

--- web.py
+++ web.py
@@ -9,6 +9,7 @@ def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ''
 
 
 st.title("My Todo App")
@@ -26,6 +27,5 @@ for index, todo in enumerate(todos):
 st.text_input(label="", placeholder="Enter a todo...",
               on_change=add_todo, key='new_todo')
 
-print("hello")
 
 
